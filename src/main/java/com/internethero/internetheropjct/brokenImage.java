cf5a0d78739dd32e205f4b9e41f1fecbc6e184a1
farahkadri <farahkad@gmail.com>
2019-07-01 23:15:49 +0530

    brokenImage is the correct testcase

--- src/main/java/com/internethero/internetheropjct/brokenImage.java
+++ src/main/java/com/internethero/internetheropjct/brokenImage.java
@@ -3,6 +3,9 @@ import static org.testng.Assert.assertEquals;
 
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,37 +16,45 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class brokenImage {
+	  static int invalidimg;
+
 	
-	
-	String image;
 	public static void main(String[] args)
 	{
 
-	System.setProperty("webdriver.chrome.driver","E:\\Drivers\\win_chromedriver.exe");
-	WebDriver driver = new ChromeDriver();
-	System.out.println("i m in chrome");
-	driver.get("https://the-internet.herokuapp.com/broken_images");
-    List <WebElement>  naturalWidth = driver.findElements(By.tagName("img"));
-
 	
-	public static void isimagebroken(String image)
-	{
-	    if (image.getAttribute("naturalWidth").equals("0"))
-	    {
-	        System.out.println(image.getAttribute("outerHTML") + " is broken.");
-
+	
+	try {
+		System.setProperty("webdriver.chrome.driver","E:\\Drivers\\win_chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		System.out.println("i m in chrome");
+		driver.get("https://the-internet.herokuapp.com/broken_images");
+  
+		invalidimg = 0;
+	      List allImages  = driver.findElements(By.tagName("img"));
+	      System.out.println("Total  images are " + allImages.size());
+	      for (int i = 0; i < allImages.size(); i++) {
+	        WebElement img = (WebElement) allImages.get(i);
+	        if (img != null) {
+	          verifyimgActive(img);
+	        }
+	      }
+	      System.out.println("Total invalid images are " + invalidimg);
+	      driver.quit();
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	      System.out.println(e.getMessage());
 	    }
-	}
-	
-	for (WebElement image : driver.findElements(By.cssSelector("img")))
-	{
-	    isImageBroken(image);
-	}
-	
-	
-	
-	
-	
+	  }
 
-}
-}
+	  public static void verifyimgActive(WebElement img) {
+	    try {
+	      HttpResponse response = new DefaultHttpClient().execute(new HttpGet(img.getAttribute("src")));
+	      if (response.getStatusLine().getStatusCode() != 200)
+	      invalidimg++;
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	    }
+	  }
+	}
+	
