b33ace3affad0bff03df8b59200c23d84d785b2d
farahkadri <farahkad@gmail.com>
2019-07-02 13:18:40 +0530

    TestNG Testcases

--- src/test/java/com/internethero/internetheropjct/tcbasicAuth.java
+++ src/test/java/com/internethero/internetheropjct/tcbasicAuth.java
@@ -1,49 +1,71 @@
 package com.internethero.internetheropjct;
 import org.testng.annotations.*;
 import java.io.File;
+
+import org.testng.Assert;
 import org.testng.ITestResult;
 import com.relevantcodes.extentreports.ExtentReports;
 import org.apache.log4j.xml.DOMConfigurator;
+import org.openqa.selenium.By;
 //import utilities.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import com.relevantcodes.extentreports.ExtentReports;
 
 
 @Test
 public class tcbasicAuth {
 
+	WebDriver driver;
+	
 
-		public void __OrangeLogin(){
-			logger = extent.startTest("_OrangeLogin");
-			Log.startTestCase("_AddContact");
+		//public void __OrangeLogin(){
+	//		logger = extent.startTest("_OrangeLogin");
+	//		Log.startTestCase("_AddContact");
 			
-		}
+	//	}
 
 
 	@BeforeMethod
-	@Parameters({"browser", "platform"})
-	public void setup(String browser, String platform) {
-		try{
-			setupBrowser(browser, platform);
-		}catch(Exception e) {}
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver","E:\\Drivers\\win_chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+
+	}
+	
+	@Test
+	public void basicAuthTest()
+	{		String actual = driver.findElement(By.cssSelector("#content > div > p")).getText();
+	Assert.assertEquals(actual, "Congratulations! You must have the proper credentials.");
+
+		
+	}
+	
+	@AfterMethod	
+	public void tearDown() {
+			driver.close();
 	}
 
-	@AfterMethod
-	public void cleanup(ITestResult result) {
-		cleanuptest(result);
-	}
 
-	@BeforeTest
-	public void startReport(){
-		extent = new ExtentReports (testoutput, false);
-		extent
-		.addSystemInfo("Host Name", " Internet ")
-		.addSystemInfo("Environment", "Assignment")
-		.addSystemInfo("User Name", "Farah Khan");
-		extent.loadConfig(new File(extentConfigutaion));
-	}
 
-	@AfterTest
-	public void endReport(){
-		extent.flush();
-		extent.close();
+//	@AfterMethod
+	//public void cleanup(ITestResult result) {
+	//	cleanuptest(result);
+	//}
+
+//	@BeforeTest
+	//public void startReport(){
+	//	extent = new ExtentReports (testoutput, false);
+	//	extent
+	//	.addSystemInfo("Host Name", " Internet ")
+	//	.addSystemInfo("Environment", "Assignment")
+	//	.addSystemInfo("User Name", "Farah Khan");
+	//	extent.loadConfig(new File(extentConfigutaion));
+	//}
+
+//	@AfterTest
+	//public void endReport(){
+	//	extent.flush();
+	//	extent.close();
 	}
-} 
